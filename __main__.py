14dadf000ab40ccc1d0dbe831ac5e04149d0835a
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-04-24 00:13:15 +0200

    building release workflows test 1

---
+++ __main__.py
@@ -0,0 +1,37 @@
+
+import fire
+from DocUtilities import *
+from ApplicationUtilities import *
+from ListUtilities import *
+from models.targets import Targets
+from termcolor import colored
+from orator.exceptions.query import QueryException
+
+
+@doc(main_docstring())
+class Sego(object):
+    def __init__(self):
+        self.list_utilities  = ListUtilities()
+        self.application_utilities = ApplicationUtilities()
+
+    @doc(ListUtilities().get_list_doc())
+    def list(self,target):
+        self.list_utilities.run_list(target=target)
+
+    @doc(ApplicationUtilities().get_application_doc())
+    def application(self,task,**kwargs):
+        self.application_utilities.run(task=task,kwargs=kwargs)
+    
+
+
+
+if __name__ == '__main__':
+    setup_utils = SetupUtilities()
+    db = DatabaseUtilities()
+    db_path = db.get_database_path()
+    home = setup_utils.get_home_dir()
+    if os.path.exists(db_path):
+        fire.Fire(Sego)
+    else:
+        setup_utils.setup()
+
