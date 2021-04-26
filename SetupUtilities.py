afca61a00ddfecc81c9cee7462e5a06408955278
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-04-27 01:35:46 +0200

    Change the env naming style to be consistent across projects

--- SetupUtilities.py
+++ SetupUtilities.py
@@ -73,4 +73,5 @@ class SetupUtilities:
         current_working_dir=os.getcwd()
         os.chdir(directory)
         subprocess.check_call(["python","-m","virtualenv",name,"--python","python3"])
+        os.rename(name,"env")
         os.chdir(current_working_dir)
