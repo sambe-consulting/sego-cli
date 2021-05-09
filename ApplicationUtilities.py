09cb46e49949a6f47f68bd6a78ab3fa83b00d9f4
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-05-10 00:36:39 +0200

    Implemented application description

--- ApplicationUtilities.py
+++ ApplicationUtilities.py
@@ -11,6 +11,8 @@ from models.application import Applications
 from prettytable import PrettyTable
 import getpass
 from orator.exceptions.orm import *
+import pyfiglet
+
 
 
 class ApplicationUtilities:
@@ -18,7 +20,7 @@ class ApplicationUtilities:
         self.app_template_url = "https://codeload.github.com/sambe-consulting/sego-app/zip/refs/heads/master"
         self.db_utils = DatabaseUtilities()
         self.setup_utils = SetupUtilities()
-        self.tasks = ["list", "generate", "delete", "register", "activate"]
+        self.tasks = ["list", "generate", "delete", "register", "activate", "describe"]
 
     def get_application_doc(self):
         doc = "The " + colored("application", "green") + " command manages application level tasks.\n"
@@ -47,12 +49,17 @@ class ApplicationUtilities:
 
         doc = doc + "\n\n The " + colored("register",
                                           "blue") + " task, registers an application to be managed by  " + colored(
-            "sego-cli", "green")+" the "+ colored("--app-dir","yellow")+" must be set to the home path of the app"
+            "sego-cli", "green") + " the " + colored("--app-dir", "yellow") + " must be set to the home path of the app"
         doc = doc + "\n\n The " + colored("activate", "blue") + " task, makes an app active, " + colored("sego-cli",
                                                                                                          "green") + " can only " \
                                                                                                                     "work on an active app, use the " + colored(
             "--name", "yellow") + " or " + colored("--id",
                                                    "yellow") + " to activate an app."                                                                                                                           ""
+        doc = doc + "\n\n The " + colored("describe",
+                                          "blue") + " task, gives configuration information for a particular application, use the " + \
+              colored("--name", "yellow") + " or " + colored("--id",
+                                                             "yellow") + " flag to specify \n the application in question. "
+
         return doc
 
     def generate_application(self, application_data):
@@ -97,10 +104,10 @@ class ApplicationUtilities:
             "updated_at": dt_string,
             "version": application_data["version"]
         }
-        virtual_env_name = application_data["app_name"]+"_env"
+        virtual_env_name = application_data["app_name"] + "_env"
         with open(configuration_path, "w") as f:
             json.dump(configurations, f, indent=4)
-        self.setup_utils.virtualenv(name=virtual_env_name,directory=directory)
+        self.setup_utils.virtualenv(name=virtual_env_name, directory=directory)
         self.db_utils.register_application(application_data)
 
     def generate(self, kwargs):
@@ -130,12 +137,14 @@ class ApplicationUtilities:
             app_data["developer"] = getpass.getuser()
         if not app_data["app_directory"].strip():
             app_data["app_directory"] = str(Path.cwd())
-        app_data["app_directory"] = app_data["app_directory"]+"/"+app_data["app_name"]
+        app_data["app_directory"] = app_data["app_directory"] + "/" + app_data["app_name"]
         exit_flag2 = True
-        while os.path.exists(app_data["app_directory"]+"/"+app_data["app_name"]) and exit_flag2:
-            print("The app directory: " + colored(app_data["app_directory"]+"/"+app_data["app_name"], "red") + " already exists")
+        while os.path.exists(app_data["app_directory"] + "/" + app_data["app_name"]) and exit_flag2:
+            print("The app directory: " + colored(app_data["app_directory"] + "/" + app_data["app_name"],
+                                                  "red") + " already exists")
             app_data["app_directory"] = input(
-            colored("Enter application directory or exit to exit:", "green") + colored("default (" + str(Path.cwd()) + "):"))
+                colored("Enter application directory or exit to exit:", "green") + colored(
+                    "default (" + str(Path.cwd()) + "):"))
         self.generate_application(application_data=app_data)
 
     def list(self, kwargs):
@@ -148,68 +157,68 @@ class ApplicationUtilities:
                 "application_identifier": app_model.application_identifier,
                 "created_at": app_model.created_at.to_datetime_string(),
                 # "updated_at": app_model.updated_at.to_datetime_string(),
-                "active":app_model.active
+                "active": app_model.active
 
             }
 
         apps = Applications.all().all()
         rows = [decompose(x) for x in apps]
 
-        headings = ["app_name", "developer", "version", "application_identifier", "created_at","active"]
+        headings = ["app_name", "developer", "version", "application_identifier", "created_at", "active"]
         table = PrettyTable(headings)
         for row in rows:
             table.add_row(list(row.values()))
 
         print(table)
 
-    def delete(self,kwargs):
+    def delete(self, kwargs):
 
-        def _delete(app,kwargs):
+        def _delete(app, kwargs):
             app_dir = str(app.app_directory)
             app.delete()
             if 'clean_up' in kwargs:
                 shutil.rmtree(app_dir)
 
-
         if 'name' in kwargs:
             name = kwargs["name"]
             try:
-                app = Applications.where('app_name','=',name).first_or_fail()
-                _delete(app,kwargs)
+                app = Applications.where('app_name', '=', name).first_or_fail()
+                _delete(app, kwargs)
             except ModelNotFound as e:
-                print(colored(e,"red"))
-                sys.exit(colored("Application with name ","red")\
-                         +colored("'"+name+"'","yellow")+colored(" is not found","red"))
+                print(colored(e, "red"))
+                sys.exit(colored("Application with name ", "red") \
+                         + colored("'" + name + "'", "yellow") + colored(" is not found", "red"))
         elif 'id' in kwargs:
             id = kwargs["id"]
             try:
-                app = Applications.where('application_identifier','=',id).first_or_fail()
-                _delete(app,kwargs)
+                app = Applications.where('application_identifier', '=', id).first_or_fail()
+                _delete(app, kwargs)
             except ModelNotFound as e:
-                print(colored(e,"red"))
-                sys.exit(colored("Application with id ","red")\
-                         +colored("'"+str(id)+"'","yellow")+colored(" is not found","red"))
+                print(colored(e, "red"))
+                sys.exit(colored("Application with id ", "red") \
+                         + colored("'" + str(id) + "'", "yellow") + colored(" is not found", "red"))
         else:
-            print(colored("Please use --name or --id to choose the application to delete","yellow"))
+            print(colored("Please use --name or --id to choose the application to delete", "yellow"))
 
-    def register(self,kwargs):
+    def register(self, kwargs):
         if 'app_dir' in kwargs:
-            to_conf ="app/Configurations/application/sego.json"
+            to_conf = "app/Configurations/application/sego.json"
             app_dir = Path(kwargs["app_dir"])
             conf_path = app_dir / Path(to_conf)
             if os.path.exists(conf_path):
                 configs = None
-                with open(conf_path,"r") as f:
+                with open(conf_path, "r") as f:
                     configs = json.loads(f.read())
                 configs["app_directory"] = str(app_dir)
                 self.db_utils.register_application(configs)
 
             else:
-                sys.exit(colored("The application must have ","red")+colored(to_conf,"yellow")+colored(" configuration file"))
+                sys.exit(colored("The application must have ", "red") + colored(to_conf, "yellow") + colored(
+                    " configuration file"))
         else:
-            print("Please use "+ colored("--app-dir","yellow")+" to specify app")
+            print("Please use " + colored("--app-dir", "yellow") + " to specify app")
 
-    def activate(self,kwargs):
+    def activate(self, kwargs):
         def _activate(app):
             all_apps = Applications.all().all()
             for app_model in all_apps:
@@ -221,23 +230,59 @@ class ApplicationUtilities:
         if 'name' in kwargs:
             name = kwargs["name"]
             try:
-                app = Applications.where('app_name','=',name).first_or_fail()
+                app = Applications.where('app_name', '=', name).first_or_fail()
                 _activate(app)
             except ModelNotFound as e:
-                print(colored(e,"red"))
-                sys.exit(colored("Application with name ","red")\
-                         +colored("'"+name+"'","yellow")+colored(" is not found","red"))
+                print(colored(e, "red"))
+                sys.exit(colored("Application with name ", "red") \
+                         + colored("'" + name + "'", "yellow") + colored(" is not found", "red"))
         elif 'id' in kwargs:
-           id = kwargs["id"]
-           try:
-             app = Applications.where('application_identifier','=',id).first_or_fail()
-             _activate(app)
-           except ModelNotFound as e:
-             print(colored(e,"red"))
-             sys.exit(colored("Application with id ","red")\
-                         +colored("'"+str(id)+"'","yellow")+colored(" is not found","red"))
+            id = kwargs["id"]
+            try:
+                app = Applications.where('application_identifier', '=', id).first_or_fail()
+                _activate(app)
+            except ModelNotFound as e:
+                print(colored(e, "red"))
+                sys.exit(colored("Application with id ", "red") \
+                         + colored("'" + str(id) + "'", "yellow") + colored(" is not found", "red"))
         else:
-            print(colored("Please use --name or --id to choose the application to activate","yellow"))
+            print(colored("Please use --name or --id to choose the application to activate", "yellow"))
+
+    def describe(self,kwargs):
+        def _describe(app):
+            app_data = app.attributes_to_dict()
+            print(colored(pyfiglet.figlet_format("--- "+app_data["app_name"]+" ---"),"green"))
+            print(colored("APPLICATION NAME:","green")+" "+colored(app_data["app_name"],"blue")+"")
+            print(colored("APPLICATION DESCRIPTION:","green")+" "+colored(app_data["description"],"blue")+"")
+            print(colored("APPLICATION DEVELOPER:","green")+" "+colored(app_data["developer"],"blue")+"")
+            print(colored("APPLICATION SOURCE DIRECTORY:","green")+" "+colored(app_data["app_directory"],"blue")+"")
+            print(colored("APPLICATION IDENTIFIER:","green")+" "+colored(app_data["application_identifier"],"blue")+"")
+            print(colored("APPLICATION VERSION:","green")+" "+colored(app_data["version"],"blue")+"")
+            print(colored("APPLICATION TYPE:","green")+" "+colored(app_data["application_type"],"blue")+"")
+            print(colored("APPLICATION CREATION DATE:","green")+" "+colored(app_data["created_at"],"blue")+"")
+            print(colored("APPLICATION ACTIVE STATUS:","green")+" "+colored(app_data["active"],"blue")+"\n")
+            print(colored(pyfiglet.figlet_format("*** "+app_data["app_name"]+" ***"),"green"))
+
+        if 'name' in kwargs:
+            name = kwargs["name"]
+            try:
+                app = Applications.where('app_name', '=', name).first_or_fail()
+                _describe(app)
+            except ModelNotFound as e:
+                print(colored(e, "red"))
+                sys.exit(colored("Application with name ", "red") \
+                         + colored("'" + name + "'", "yellow") + colored(" is not found", "red"))
+        elif 'id' in kwargs:
+            id = kwargs["id"]
+            try:
+                app = Applications.where('application_identifier', '=', id).first_or_fail()
+                _describe(app)
+            except ModelNotFound as e:
+                print(colored(e, "red"))
+                sys.exit(colored("Application with id ", "red") \
+                         + colored("'" + str(id) + "'", "yellow") + colored(" is not found", "red"))
+        else:
+            print(colored("Please use --name or --id to choose the application to describe", "yellow"))
 
 
     def run(self, task, kwargs):
