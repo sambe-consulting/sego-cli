c38702412f1dee04c0baededc6665c0271580922
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-05-10 04:10:17 +0200

    Completed controller generation

--- ControllerUtilities.py
+++ ControllerUtilities.py
@@ -7,12 +7,19 @@ from contextlib import contextmanager
 import importlib
 from uuid import uuid4
 import pyfiglet
+import io
+import getpass
+from CodeGenerationUtilities import CodeGenerationUtilities
+from datetime import datetime
+
 
 
 class ControllerUtilities:
     def __init__(self):
         self.tasks = ["list", "generate", "delete"]
         self.active_app = None
+        self.known_controllers = []
+        self.code_generator = CodeGenerationUtilities()
 
     def get_controller_doc(self):
         doc = "The " + colored("controller", "green") + " command manages controller level tasks." \
@@ -50,7 +57,7 @@ class ControllerUtilities:
             for line in oldfile:
                 if not any(bad_word in line for bad_word in bad_words):
                     if 'BaseController' in line:
-                        line = line.split("(")[0] + ":"
+                        line = line.split("(")[0] + ":\n"
                         newfile.write(line)
                     else:
                         newfile.write(line)
@@ -65,29 +72,30 @@ class ControllerUtilities:
 
         self.active_app = Applications.where('active', '=', 1).first_or_fail()
         self.active_app_dict = self.active_app.attributes_to_dict()
+        app_directory = Path(self.active_app_dict["app_directory"]) / "app/Controllers"
+        root_directory = Path(self.active_app_dict["app_directory"])
+        sys.path.append(str(root_directory))
+        self.all_files = []
+        for root, subdirs, files in os.walk(app_directory):
+            if '__pycache__' not in root:
+                temp_root = Path(root)
+                self.all_files = self.all_files + [temp_root / file for file in files if file != '__init__.py']
 
     def list(self, kwargs):
         self.set_active()
         print(colored("*","yellow")*80)
         print(colored(pyfiglet.figlet_format("--- CONTROLLERS ---"), "green"))
-        app_directory = Path(self.active_app_dict["app_directory"]) / "app/Controllers"
-        root_directory = Path(self.active_app_dict["app_directory"])
-        sys.path.append(str(root_directory))
-        current_wd = os.getcwd()
-        all_files = []
-        for root, subdirs, files in os.walk(app_directory):
-            if '__pycache__' not in root:
-                temp_root = Path(root)
-                all_files = all_files + [temp_root / file for file in files if file != '__init__.py']
+
         print(colored("=", "yellow") * 80)
 
-        for file in all_files:
+        for file in self.all_files:
             new_name = str(uuid4())+".py"
             self.cleanup(file,new_name)
             module = self.path_import(new_name)
             for name,obj in inspect.getmembers(module):
                 try:
                     if inspect.isclass(obj):
+                        self.known_controllers.append(name)
                         print(colored("CONTROLLER NAME: ","green")+" "+colored(name,"blue"))
                         functions = inspect.getmembers(obj,predicate=inspect.isfunction)
                         for function_name,function in functions:
@@ -105,10 +113,38 @@ class ControllerUtilities:
         print("\n")
         print(colored("*","yellow")*80)
 
-
-
     def generate(self, kwargs):
-        pass
+        save_stdout = sys.stdout
+        sys.stdout = open('trash', 'w')
+        self.list(kwargs)
+        sys.stdout = save_stdout
+
+        ctrl_data = {}
+        controller_path = Path(self.active_app_dict["app_directory"])/"app/Controllers"
+        print(controller_path)
+        exit_flag = True
+        ctrl_data["name"] = input(colored("Please enter controller name ","green")+colored("e.g HomeController : ","yellow"))
+        while ctrl_data["name"] in self.known_controllers and exit_flag:
+            print("The controller name: " + colored(ctrl_data["name"], "red") + " already exists")
+            ctrl_data["name"] = input(colored("Enter controller name or type exit to exit : ", "green"))
+            if ctrl_data["name"] == 'exit':
+                exit_flag = False
+                sys.exit("EXITING !!!")
+        ctrl_data["controller_description"] = input(colored("Please enter a short description of the controller:","green"))
+        ctrl_data["author"] =  input(
+            colored("Enter controller author ", "green") + colored("default (" + getpass.getuser() + "):"))
+        ctrl_data["author_email"] = input(colored("Enter the email address of the author/maintainer: ","green"))
+        ctrl_data["version"] = input(colored("Enter the version of this controller: ","green"))
+        ctrl_data["controller_title"] = ctrl_data["name"]
+        ctrl_data["controller_name"] = ctrl_data["name"]
+        now = datetime.now()
+        ctrl_data["generation_date"] = str(now.strftime("%d/%m/%Y %H:%M:%S"))
+        results = self.code_generator.generate('controller.tpl',ctrl_data)
+        filename =ctrl_data["name"]+".py"
+        controller_file = controller_path/filename
+        with open(controller_file,"w") as f:
+            f.write(results)
+        print(results)
 
     def delete(self, kwargs):
         pass
